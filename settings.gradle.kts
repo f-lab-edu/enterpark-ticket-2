337062dafdeb56e1a9fa840178ef9a0d00287064
YuGyeong98 <kuk3097@naver.com>
2025-01-14 15:49:17 +0900

    #1 [chore] 프로젝트 멀티모듈 설정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
 rootProject.name = "enterparkticket"
+
+include(
+    "enterpark-ticket-admin",
+    "enterpark-ticket-apis",
+    "enterpark-ticket-application",
+    "enterpark-ticket-apis:creator",
+    "enterpark-ticket-apis:enduser",
+    "enterpark-ticket-batch",
+    "enterpark-ticket-domain",
+    "enterpark-ticket-infra",
+)
