f166a337dca8e080c459ad83a1b3072dc4ce797c
YuGyeong98 <kuk3097@naver.com>
2025-01-22 19:37:04 +0900

    #3 [chore] actuator, prometheus 라이브러리 추가

--- enterpark-ticket-apis/enduser/build.gradle.kts
+++ enterpark-ticket-apis/enduser/build.gradle.kts
@@ -11,6 +11,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
